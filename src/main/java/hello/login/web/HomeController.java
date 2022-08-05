30a61f10c2a46cb55180034ea46a973c29adad25
OldRabbit736 <sylvan0212@gmail.com>
2022-08-05 11:19:22 +0900

    완로: 로그인 처리하기 - 서블릿 HTTP 세션2

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -28,6 +29,18 @@ public class HomeController {
 
     // 로그인 정보까지 처리 되는 홈 화면
     @GetMapping("/")
+    public String homeLoginV3Spring(
+            // 이미 로그인 된 사용자를 찾을 때 이렇게 사용. 세션은 생성하지 않는다.
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+
+        if (loginMember == null) return "home";
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    //@GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // session 은 메모리를 사용하기 때문에 꼭 필요한 경우에만 생성하자.
@@ -35,7 +48,7 @@ public class HomeController {
         if (session == null) return "home";
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
-        if(loginMember == null) return "home";
+        if (loginMember == null) return "home";
 
         model.addAttribute("member", loginMember);
         return "loginHome";
